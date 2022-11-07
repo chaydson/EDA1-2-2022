8ea87e43802ebbc39ea3bc76d80677022506dff1
chaydson <chaydson14@gmail.com>
2022-11-07 14:04:15 -0300

    added B1 solution

--- lista-2/B1.c
+++ lista-2/B1.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-int main(){
-    printf("%d", faz_conta_direito(4, '-'));
-}
-
 int faz_conta_direito(int parcelas, char op){
     int soma = 0, numero = 0;
-    scanf("%d %s", &parcelas, &op);
     if(op == '+'){
         for(int i = 0; i < parcelas; i++){
             scanf("%d", &numero);
