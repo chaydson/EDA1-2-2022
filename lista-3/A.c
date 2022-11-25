680f201cefa2325aac12ac3c1bbe667b92e3f8f6
chaydson <chaydson14@gmail.com>
2022-11-24 22:56:38 -0300

    added l3 a start

---
+++ lista-3/A.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int f91(int n){
+    if(n >= 101) return n-10;
+    if(n <= 100) {
+        f91(f91(n+11));
+    }
+}
+
+int main(){
+    int n_main = 1;
+    while (scanf("%d", &n_main) != 0){
+        if(n_main == 0) return;
+        printf("f91(%d) = %d \n", n_main ,f91(n_main));
+    }
+    
+    return 0;
+}
