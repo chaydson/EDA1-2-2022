6d3ed5c4fc6de01ad1303be61e80622ba15b8945
chaydson <chaydson14@gmail.com>
2022-11-27 14:27:48 -0300

    added pre l3 c solution

--- lista-3/C.c
+++ lista-3/C.c
@@ -1,6 +1,24 @@
+int soma(int n){
+    if(n <= 0) return 0;
+    return n%10 + soma(n/10);;
+}
 
+int resultado(int n){
+    int soma_resultado = soma(n);
+    int aux = soma_resultado;
+    int grau = 0;
+    while(aux > 0){
+        aux = soma_resultado/10;
+        grau++;
+    }
+
+    if(soma_resultado%9 == 0){
+        printf("%d is a multiple of and has a 9-degree %d.\n", n, grau);
+    } else{
+        printf("%d is not a multiple of 9.\n", n);
+    } 
+}
 
 int main(){
 
-    return 0;
 }
