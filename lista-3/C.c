fd0cacd75914e707be5365a8561d24bdecdcaddf
chaydson <chaydson14@gmail.com>
2022-11-27 23:27:34 -0300

    added l3 C solution

--- lista-3/C.c
+++ lista-3/C.c
@@ -1,24 +1,40 @@
-int soma(int n){
-    if(n <= 0) return 0;
-    return n%10 + soma(n/10);;
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int resultado(int n){
-    int soma_resultado = soma(n);
-    int aux = soma_resultado;
-    int grau = 0;
-    while(aux > 0){
-        aux = soma_resultado/10;
-        grau++;
+int prof = 1;
+int soma(char numero[1001]){
+    char aux[1001];
+    int s = 0;
+    for(int i = 0; i < strlen(numero); i++){
+        s += (int)numero[i] - 48;
     }
 
-    if(soma_resultado%9 == 0){
-        printf("%d is a multiple of and has a 9-degree %d.\n", n, grau);
-    } else{
-        printf("%d is not a multiple of 9.\n", n);
-    } 
+    if(s == 9) {
+        return 1;
+    }
+
+    if(s%9 != 0) {
+        return 0;
+    }
+
+    if(s > 9) {
+        prof++;
+        sprintf(aux, "%d", s);
+        soma(aux);
+    }
 }
 
 int main(){
-
+    char numero_main[1001];
+    int resultado = 0;
+    scanf("%s", &numero_main);
+    while(numero_main[0] != '0'){
+        resultado = soma(numero_main);
+        if(resultado == 0) printf("%s is not a multiple of 9.\n", numero_main);
+        else printf("%s is a multiple of 9 and has 9-degree %d.\n", numero_main, prof);
+        prof = 1;
+        scanf("%s", &numero_main);
+    }
+    return 0;
 }
